521391e7cc23103410520488d8d2027c18e3d459
Guillermo Hernandez <frontend@reddesignsystems.com>
2016-03-07 12:37:51 -0500

    Structure project

---
+++ app.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require ('react-dom');
+
+ReactDOM.render(
+	<div>
+	  <h1>Hola Mundo React</h1>
+	  <h1>buen dia !!!!</h1>
+	</div>,
+	document.getElementById('container')
+);
